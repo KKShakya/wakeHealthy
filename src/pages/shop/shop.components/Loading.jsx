6b40e5a696728797ca11f8152184e6a2772f5ebe
nikhil <angolkarnikhil@gmail.com>
2022-12-14 02:46:14 +0530

    day-2

--- src/pages/shop/shop.components/Loading.jsx
+++ src/pages/shop/shop.components/Loading.jsx
@@ -1,9 +1,9 @@
-import { Box, Heading, Image, Skeleton, Stack } from "@chakra-ui/react";
+import { Image, Stack } from "@chakra-ui/react";
 import React from "react";
 
 export default function Loading() {
   return (
-    <Stack height="100vh">
+    <Stack height="100vh" justify={"center"}>
       <Image src="https://miro.medium.com/max/1100/1*r4K1PRHfbKG7NpoRx22K4A.gif" />
     </Stack>
   );
