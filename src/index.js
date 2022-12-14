2c35ca9c2368830e8a1df34eedbf5266a96286c9
Krishna Kumar Shakya <Krishnashakya7272@gmail.com>
2022-12-14 22:22:19 +0530

    Update index.js
    Added chakra provider

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./store/Store";
 import { BrowserRouter } from "react-router-dom";
-
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
