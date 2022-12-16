a9fb68025473bed629770de75dc150a48f302f27
Mohammad Sahil <starlordz956@gmail.com>
2022-12-16 21:19:16 +0530

    added day-5 work

--- src/pages/Homepage/Middle3.jsx
+++ src/pages/Homepage/Middle3.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Middle3.css'
 const Middle3 = () => {
+
+  
   return (
    <div className='flex123'>
     <div>
@@ -10,7 +12,7 @@ const Middle3 = () => {
     </div>
     <div>
     <div data-aos="fade-up">
-        <img src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_630,q_auto:eco,dpr_1,f_auto,fl_progressive/image/test/image_zoom_widget/image_zoom_widget_img_4.png" alt="" />
+        <img style={{marginTop:'66px'}} src="https://cdn-images.cure.fit/www-curefit-com/image/upload/c_fill,w_630,q_auto:eco,dpr_1,f_auto,fl_progressive/image/test/image_zoom_widget/image_zoom_widget_img_4.png" alt="" />
 </div>
     </div>
     <div>
