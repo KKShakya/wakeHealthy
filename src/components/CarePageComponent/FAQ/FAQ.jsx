33f14866a4cbb068d2350975b79b0b8a0836d372
Krishna Kumar Shakya <Krishnashakya7272@gmail.com>
2022-12-17 22:00:41 +0530

    Update FAQ.jsx

--- src/components/CarePageComponent/FAQ/FAQ.jsx
+++ src/components/CarePageComponent/FAQ/FAQ.jsx
@@ -1,64 +1,64 @@
-// // // this FAQ  Component to do by Sumit ss
+// this FAQ  Component to do by Sumit ss
 
 
-// import React from 'react'
-// import axios from "axios"
-// import "./FAQ.css"
-// import {Accordion, Box, Text} from '@chakra-ui/react'
-// import Accordion_Item from './Accordion_Item'
+ import React from 'react'
+ import axios from "axios"
+ import "./FAQ.css"
+ import {Accordion, Box, Text} from '@chakra-ui/react'
+ import Accordion_Item from './Accordion_Item'
 
 
-// const FAQ = () => {
+ const FAQ = () => {
 
-//    const [AccordionItems ,SetAccordionItem]=React.useState([])
+    const [AccordionItems ,SetAccordionItem]=React.useState([])
 
-//    React.useEffect(()=>{
+   React.useEffect(()=>{
            
-//     axios.get('http://localhost:8080/FAQ_DATA').then((data)=>SetAccordionItem(data.data))
+    axios.get('http://localhost:8080/FAQ_DATA').then((data)=>SetAccordionItem(data.data))
 
-//    },[])
+   },[])
  
 
 
 
-//    return (
-//      <div className='faq_div'>
+    return (
+      <div className='faq_div'>
             
 
-//              <Box className='Faq_heading' h={{ "base": '60px', "md": '90px', "lg": '110px' }}>
-//                  <Text  fontSize={{ "base": '18px', "md": '22px', "lg": '30px' }}><b>FAQS</b> </Text>
-//                  <Text fontSize={{ "base": '10px', "md": '15px', "lg": '20px' }} color="#6A6A6A">7 Question</Text>             </Box>
+              <Box className='Faq_heading' h={{ "base": '60px', "md": '90px', "lg": '110px' }}>
+                  <Text  fontSize={{ "base": '18px', "md": '22px', "lg": '30px' }}><b>FAQS</b> </Text>
+                  <Text fontSize={{ "base": '10px', "md": '15px', "lg": '20px' }} color="#6A6A6A">7 Question</Text>             </Box>
 
 
-//              <div className='Accordion'>
-//                      <Accordion allowToggle>
-//                          {
-//                              FAQ_DATA.map((item)=>(
-//                                <Accordion_Item  item={item} key={Math.random()*100000}/>
-//                             ))
+              <div className='Accordion'>
+                      <Accordion allowToggle>
+                          {
+                              FAQ_DATA.map((item)=>(
+                                <Accordion_Item  item={item} key={Math.random()*100000}/>
+                             ))
 
-//             <Box className='Faq_heading' h={{ "base": '60px', "md": '90px', "lg": '110px' }}>
-//                 <Text  fontSize={{ "base": '18px', "md": '22px', "lg": '30px' }}><b>FAQS</b> </Text>
-//                 <Text fontSize={{ "base": '10px', "md": '15px', "lg": '20px' }} color="#6A6A6A">7 Question</Text>
-//             </Box>
+             <Box className='Faq_heading' h={{ "base": '60px', "md": '90px', "lg": '110px' }}>
+                 <Text  fontSize={{ "base": '18px', "md": '22px', "lg": '30px' }}><b>FAQS</b> </Text>
+                 <Text fontSize={{ "base": '10px', "md": '15px', "lg": '20px' }} color="#6A6A6A">7 Question</Text>
+             </Box>
 
 
-//             <div className='Accordion'>
-//                     <Accordion allowToggle>
-//                         {
-//                             AccordionItems.map((item)=>(
-//                               <Accordion_Item  item={item} key={Math.random()*100000}/>
-//                             ))
-//                         }
+             <div className='Accordion'>
+                     <Accordion allowToggle>
+                         {
+                             AccordionItems.map((item)=>(
+                               <Accordion_Item  item={item} key={Math.random()*100000}/>
+                             ))
+                         }
 
                        
         
-//                      </Accordion>
+                      </Accordion>
             
-//             </div>
+             </div>
 
-//          </div>
-//    )
-//  }
+          </div>
+   )
+  }
 
-//  export default FAQ
+  export default FAQ
